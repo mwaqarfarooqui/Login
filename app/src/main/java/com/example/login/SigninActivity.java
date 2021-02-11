4c13ab17056e4bf3c94eabb9db4a2ae913eddd5a
wasee95 <swasimjan260@gmail.com>
2021-02-11 16:01:13 +0500

    * Changed these Activitie names.

--- app/src/main/java/com/example/login/SigninActivity.java
+++ app/src/main/java/com/example/login/SigninActivity.java
@@ -1,14 +1,14 @@
 package com.example.login;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class Signup extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SigninActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_signin);
     }
 }
